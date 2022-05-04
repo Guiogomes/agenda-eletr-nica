e40178e751d4426217633ebf77a7396e4c8c2fe3
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-04 17:40:50 -0300

    feat: cria os metodos básicos para realização de um CRUD

---
+++ app/backend/src/services/todosService.js
@@ -0,0 +1,39 @@
+const { Todos } = require('../database/models');
+
+const create = async (data) => {
+  const newTodo = await Todos.create(data);
+  return newTodo;
+};
+
+const read = async () => {
+  const todos = await Todos.findAll();
+  return todos;
+};
+
+const readOne = async (id) => {
+  const todo = await Todos.findByPk(id);
+  return todo;
+};
+
+const update = async (id, data) => {
+  const todo = await Todos.findByPk(id);
+  if(!todo) throw new Error('Todo not found');
+  todo.set({ ...data });
+  await todo.save();
+  return todo;
+};
+
+const deleteTodo = async (id) => {
+  const todo = await Todos.findByPk(id);
+  if(!todo) throw new Error('Todo not found');
+  await todo.destroy();
+  return todo;
+};
+
+module.exports = {
+  create,
+  read,
+  readOne,
+  update,
+  deleteTodo,
+};
