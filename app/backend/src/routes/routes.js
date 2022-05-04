b0f7a86340defd60c9c0397cb0a8da0e54d41d08
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-04 18:01:14 -0300

    feat: implementa as rotas

--- app/backend/src/routes/routes.js
+++ app/backend/src/routes/routes.js
@@ -1,5 +1,12 @@
 const Router = require('express');
+const todosController = require('../controllers/todosController');
 
 const router = Router();
+
+router.get('/agenda', todosController.read);
+router.get('/agenda/:id', todosController.readOne);
+router.post('/agenda', todosController.create);
+router.put('/agenda/:id', todosController.update);
+router.delete('/agenda/:id', todosController.deleteTodo);
 
 module.exports = router;
