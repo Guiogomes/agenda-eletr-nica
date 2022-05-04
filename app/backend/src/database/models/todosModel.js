4491272f8e5ec3f8f30d577aa52d896742662a05
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-04 19:14:41 -0300

    fix: resolve o problema de tipagem da hora

--- app/backend/src/database/models/todosModel.js
+++ app/backend/src/database/models/todosModel.js
@@ -1,22 +1,25 @@
 module.exports = (sequelize, DataTypes) =>{
-  const Todos = sequelize.define('todos', {
+  const Todos = sequelize.define('Todos', {
     Nome: {
       allowNull: false,
       type: DataTypes.STRING,
     },
     Data: {
       allowNull: false,
-      default: DataTypes.NOW,
-      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      type: DataTypes.DATEONLY,
     },
     Hora: {
-      allowNull: false,
-      type: DataTypes.TIME,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.DATE,
     },
-    Título: {
+    Titulo: {
       allowNull: false,
       type: DataTypes.STRING({ length: 100 }),
     }
+  }, {
+    tableName: 'todos',
+    timestamps: false,
   })
 
   return Todos;
