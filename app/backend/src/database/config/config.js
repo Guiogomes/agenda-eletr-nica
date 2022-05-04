3f7a985d52c7f1712732ad81fb35c4b47a82f7f7
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-04 19:13:08 -0300

    refactor: simplefica o config.js

--- app/backend/src/database/config/config.js
+++ app/backend/src/database/config/config.js
@@ -1,37 +1,30 @@
+
+  
 require('dotenv').config();
-
-const environment = process.env.NODE_ENV || "test";
-
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
-
-const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'agenda-eletronica'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
-};
 
 module.exports = {
   development: {
-    ...options,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'agenda_eletronica',
+    port: process.env.MYSQL_PORT,
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
   },
   test: {
-    ...options,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'agenda_eletronica',
+    port: process.env.MYSQL_PORT,
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
   },
   production: {
-    ...options,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'agenda_eletronica',
+    port: process.env.MYSQL_PORT,
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
   },
 };
