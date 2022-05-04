9919404ebf05710cb60c5070a52b32a4d8bc5388
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-04 20:02:58 -0300

    feat: cria a pasta utils e o arquivo de requests, implementa o metodo de criar uma tarefa

---
+++ app/frontend/src/utils/Request.js
@@ -0,0 +1,12 @@
+const url = process.env.REACT_APP_HOSTNAME || 'http://localhost:3001';
+const contentType = 'application/json; charset=utf-8';
+
+const createTask = async (data) => {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': contentType },
+    body: JSON.stringify({ ...data }),
+  };
+  const response = await fetch(`${url}/agenda`, options)
+  return response
+};
