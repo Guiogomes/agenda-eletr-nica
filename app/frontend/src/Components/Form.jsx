2cc63df0238b80e3994d2ddc6368bbf9f81e5afe
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-05 15:20:08 -0300

    refactor: tira a lógica do forms que será renderizado para os forms normal e editado

--- app/frontend/src/Components/Form.jsx
+++ app/frontend/src/Components/Form.jsx
@@ -1,70 +1,16 @@
 import React, { useContext } from 'react';
 import { MyContext } from '../context/Provider';
+import EditedForm from './EditedForm';
+import RegularForm from './RegularForm';
 
 
 const Form = () => {
-  const {
-    Nome,
-    setNome,
-    Data,
-    setData,
-    Hora,
-    setHora,
-    Titulo,
-    setTitulo,
-    setToDo,
-  } = useContext(MyContext);
+  const { isEdited } = useContext(MyContext);
 
   return(
-    <form>
-      <label>
-        Nome:
-        <input
-          name={ Nome }
-          type='text'
-          placeholder='Nome da tarefa'
-          onChange={(e) => setNome(e.target.value)}
-          value={ Nome }
-        />
-      </label>
-      <label>
-        Data:
-        <input
-          name={ Data }
-          type='date'
-          placeholder='Data de realização'
-          onChange={(e) => setData(e.target.value)}
-          value={ Data }
-        />
-      </label>
-      <label>
-        Hora:
-        <input
-          name={ Hora }
-          type='hour'
-          placeholder='Horário previsto'
-          onChange={(e) => setHora(e.target.value)}
-          value={ Hora }
-        />
-      </label>
-      <label>
-        Titulo:
-        <textarea
-          name={ Titulo }
-          type='text'
-          placeholder='Breve descrição'
-          maxLength={ 100 }
-          onChange={(e) => setTitulo(e.target.value)}
-          value={ Titulo }
-        />
-      </label>
-      <button
-        type='button'
-        onClick={() => setToDo()}
-      >
-        Criar
-      </button>
-    </form>
+    <div>
+      {isEdited ? <EditedForm/> : <RegularForm/>}
+    </div>
   )
 };
 
