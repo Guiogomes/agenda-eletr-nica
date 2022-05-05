f1b07dbd95cfb40dd5b3f78c7d6c84cbcaba65a0
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-05 20:37:53 -0300

    feat: adiciona o css no header

--- app/frontend/src/Components/Headers.jsx
+++ app/frontend/src/Components/Headers.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../css/Headers.css';
 
 const Headers = () => {
   return (
-    <header>
+    <header className='header-containe'>
       <h1>Agenda Eletrônica</h1>
     </header>
   );
