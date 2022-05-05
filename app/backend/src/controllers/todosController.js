17b152155aec443651990936c21df99a7e82c455
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-05 09:33:11 -0300

    refactor: coloca o código em um bloco try/catch

--- app/backend/src/controllers/todosController.js
+++ app/backend/src/controllers/todosController.js
@@ -1,28 +1,49 @@
 const todosService = require('../services/todosService');
 
 const create = async (req, res) => {
-  const newTodo = await todosService.create(req.body);
-  return res.status(201).json(newTodo);
+  try {
+    const newTodo = await todosService.create(req.body);
+    return res.status(201).json(newTodo);
+  } catch(e) {
+    return res.status(500).json({error: e.message});
+  }
+
 };
 
 const read = async (req, res) => {
-  const todos = await todosService.read();
-  return res.status(200).json(todos);
+  try {
+    const todos = await todosService.read();
+    return res.status(200).json(todos);
+  } catch(e) {
+    return res.status(500).json({error: e.message});
+  }
 }
 
 const readOne = async (req, res) => {
-  const todo = await todosService.readOne(req.params.id);
-  return res.status(200).json(todo);
+  try {
+    const todo = await todosService.readOne(req.params.id);
+    return res.status(200).json(todo);
+  } catch(e) {
+    return res.status(404).json({error: e.message});
+  }
 }
 
 const update = async (req, res) => {
-  const todo = await todosService.update(req.params.id, req.body);
-  return res.status(200).json(todo);
+  try {
+    const todo = await todosService.update(req.params.id, req.body);
+    return res.status(200).json(todo);
+  } catch(e) {
+    return res.status(404).json({error: e.message});
+  }
 };
 
 const deleteTodo = async (req, res) => {
-  const todo = await todosService.deleteTodo(req.params.id);
-  return res.status(200).json(todo);
+  try{
+    const todo = await todosService.deleteTodo(req.params.id);
+    return res.status(200).json(todo);
+  } catch(e) {
+    return res.status(404).json({error: e.message});
+  }
 }
 
 module.exports = {
