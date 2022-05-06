d63b2dac7d5222bd69729e2b4c955bb412a02e7b
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-06 15:28:32 -0300

    conflict: resolve conflitos do package.json e muda o dialect de production pra postgres

--- app/backend/src/database/config/config.js
+++ app/backend/src/database/config/config.js
@@ -20,11 +20,12 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
+    use_env_variable: 'DATABASE_URL',
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB_NAME,
     port: process.env.MYSQL_PORT,
     host: process.env.MYSQL_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
 };
